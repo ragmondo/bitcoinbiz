5021366fcebb014835f1339b50e239eeae3077ed
Richard Green <richard@barcodebeasties.com>
2013-03-18 14:18:22 +0000

    removed functions that aren't used

--- src/com/ragmondo/NetworkBits.java
+++ src/com/ragmondo/NetworkBits.java
@@ -1,11 +1,14 @@
 package com.ragmondo;
 
 import android.content.Context;
-import android.net.Uri;
 import android.util.Log;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Map;
 
 /**
@@ -171,147 +174,6 @@ public class NetworkBits {
         return response;
     }
 
-
-
-    public static Uri DownloadRingtoneFromUrl(File dest, URL u, String localname) {
-
-        Log.d(Tag, "File dest is " + dest);
-        Log.d(Tag, "URl u is " + u);
-        Log.d(Tag, "localname is " + localname);
-
-        Uri ret = null;
-        HttpURLConnection c = null;
-        Log.d(Tag, "download started..");
-        try {
-            c  = (HttpURLConnection) u.openConnection();
-            c.setRequestMethod("GET");
-            //c.setDoOutput(true); this would make a POST request if set.
-            c.connect();
-            Log.d(Tag, "Connected.");
-        }
-     catch (ProtocolException e) {
-            Log.e(Tag, "Exception", e);  //To change body of catch statement use File | Settings | File Templates.
-        } catch (IOException e) {
-            Log.e(Tag, "Exception", e);  //To change body of catch statement use File | Settings | File Templates.
-        }
-        File file = new File(dest, localname);
-
-        if (file.exists()) {
-            Log.d(Tag, "Overwriting old filename");
-            file.delete();
-            try {
-                file = new File(dest, localname);
-                file.createNewFile();
-            } catch (IOException e) {
-                Log.e(Tag, "Exception", e);  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
-
-        try {
-            Log.d(Tag, "File is " + file.getCanonicalPath().toString());
-            Log.d(Tag, "Writing to " + file.getAbsolutePath());
-            FileOutputStream f = null;
-            try{
-            f = new FileOutputStream(file);
-            }
-            catch (FileNotFoundException e) {
-                Log.e(Tag, "WEIRD Exception - I JUST CREATED IT SECONDS AGO...", e);  //To change body of catch statement use File | Settings | File Templates.
-            }
-
-            InputStream in = c.getInputStream();
-
-            byte[] buffer = new byte[8192];
-            int len1 = 0;
-            while ((len1 = in.read(buffer)) > 0) {
-                //Log.d(Tag,"downloading..");
-            //    Log.d(Tag, "Writing " + len1 + " bytes");
-                f.write(buffer, 0, len1);
-            }
-            f.close();
-        } catch (FileNotFoundException e) {
-            Log.e(Tag, "Exception", e);  //To change body of catch statement use File | Settings | File Templates.
-        } catch (IOException e) {
-            Log.e(Tag, "Exception", e);  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-        ret = Uri.parse(file.getAbsolutePath());
-        if (ret == null) {
-            Log.d(Tag, "ret is null :-(");
-        } else {
-            Log.d(Tag, "ret is " + ret);
-        }
-        Log.d(Tag, "download finished..");
-
-        return ret;
-
-    }
-
-    /*
-
-    public static String uploadAudioToServer(String surl, Uri uri, Context c) throws IOException {
-        InputStream is = c.getContentResolver().openInputStream(uri);
-        File outputDir = c.getCacheDir(); // context being the Activity pointer
-        File outputFile = File.createTempFile("ringtone", "mp3", outputDir);
-        FileOutputStream f = new FileOutputStream(outputFile);
-            byte[] buffer = new byte[8192];
-            int len1 = 0;
-            while ((len1 = is.read(buffer)) > 0) {
-                //Log.d(Tag,"downloading..");
-                //Log.d(Tag,"Writing "+len1 + " bytes");
-                f.write(buffer, 0, len1);
-            }
-            f.close();
-        Log.d(Tag, "Uploading started..");
-        HttpResponse response;
-        Log.d(Tag, " byte length is  " + outputFile.length());
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(surl);
-
-            FileBody bin = new FileBody(outputFile);
-            StringBody comment = new StringBody("Filename: " + outputFile.getName());
-
-            MultipartEntity reqEntity = new MultipartEntity();
-            reqEntity.addPart("bin", bin);
-            reqEntity.addPart("comment", comment);
-            httppost.setEntity(reqEntity);
-
-            response = httpclient.execute(httppost);
-            HttpEntity resEntity = response.getEntity();
-            String resp = EntityUtils.toString(resEntity);
-             Log.d(Tag, "resp was " + resp);
-        return resp;
-    }
-
-*/
-
-      /*
-    public static String uploadAudioToServer(String surl, String filename, ContentResolver cr) throws IOException {
-
-        //ParcelFileDescriptor pfd = getCon
-
-        Log.d(Tag, "Uploading started..");
-        File file = new File(filename);
-        HttpResponse response;
-        Log.d(Tag, " byte length is  " + file.length());
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost(surl);
-
-        FileBody bin = new FileBody(file);
-        StringBody comment = new StringBody("Filename: " + file.getName());
-
-        MultipartEntity reqEntity = new MultipartEntity();
-        reqEntity.addPart("bin", bin);
-        reqEntity.addPart("comment", comment);
-        httppost.setEntity(reqEntity);
-
-        response = httpclient.execute(httppost);
-        HttpEntity resEntity = response.getEntity();
-        String resp = EntityUtils.toString(resEntity);
-        Log.d(Tag, "resp was " + resp);
-        return resp;
-    }
-
-        */
 
 
 
